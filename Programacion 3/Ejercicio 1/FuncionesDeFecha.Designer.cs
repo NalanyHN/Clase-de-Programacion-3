54387f85e276df52d1943eb109df5ef22c021dc0
NalanyHN <nalarock16@gmail.com>
2021-09-14 14:55:49 -0600

    Funciones de fecha
    Funciones de fecha

---
+++ Programacion 3/Ejercicio 1/FuncionesDeFecha.Designer.cs
@@ -0,0 +1,342 @@
+﻿
+namespace Ejercicio_1
+{
+    partial class FuncionesDeFecha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Dtp_Fecha = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_Ejecutar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txt_Numerodia = new System.Windows.Forms.TextBox();
+            this.Txt_Numeromes = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Txt_año = new System.Windows.Forms.TextBox();
+            this.Txt_Hora = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Txt_Meslestras = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Txt_Dialetras = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Txt_Agregardias = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Txt_Nuevafecha = new System.Windows.Forms.TextBox();
+            this.Txt_Nuevafechamenos = new System.Windows.Forms.TextBox();
+            this.Txt_Restardias = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Lbl_Hora = new System.Windows.Forms.Label();
+            this.Txt_Edad = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Dtp_Fecha
+            // 
+            this.Dtp_Fecha.Location = new System.Drawing.Point(122, 59);
+            this.Dtp_Fecha.Name = "Dtp_Fecha";
+            this.Dtp_Fecha.Size = new System.Drawing.Size(200, 20);
+            this.Dtp_Fecha.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Fecha";
+            // 
+            // Btn_Ejecutar
+            // 
+            this.Btn_Ejecutar.Location = new System.Drawing.Point(123, 120);
+            this.Btn_Ejecutar.Name = "Btn_Ejecutar";
+            this.Btn_Ejecutar.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Ejecutar.TabIndex = 2;
+            this.Btn_Ejecutar.Text = "Ejecutar";
+            this.Btn_Ejecutar.UseVisualStyleBackColor = true;
+            this.Btn_Ejecutar.Click += new System.EventHandler(this.Btn_Ejecutar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numero De Dia";
+            // 
+            // Txt_Numerodia
+            // 
+            this.Txt_Numerodia.Enabled = false;
+            this.Txt_Numerodia.Location = new System.Drawing.Point(148, 174);
+            this.Txt_Numerodia.Name = "Txt_Numerodia";
+            this.Txt_Numerodia.ReadOnly = true;
+            this.Txt_Numerodia.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Numerodia.TabIndex = 4;
+            // 
+            // Txt_Numeromes
+            // 
+            this.Txt_Numeromes.Enabled = false;
+            this.Txt_Numeromes.Location = new System.Drawing.Point(148, 215);
+            this.Txt_Numeromes.Name = "Txt_Numeromes";
+            this.Txt_Numeromes.ReadOnly = true;
+            this.Txt_Numeromes.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Numeromes.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Numero De Mes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = " Año";
+            // 
+            // Txt_año
+            // 
+            this.Txt_año.Enabled = false;
+            this.Txt_año.Location = new System.Drawing.Point(148, 261);
+            this.Txt_año.Name = "Txt_año";
+            this.Txt_año.ReadOnly = true;
+            this.Txt_año.Size = new System.Drawing.Size(100, 20);
+            this.Txt_año.TabIndex = 8;
+            // 
+            // Txt_Hora
+            // 
+            this.Txt_Hora.Enabled = false;
+            this.Txt_Hora.Location = new System.Drawing.Point(153, 313);
+            this.Txt_Hora.Name = "Txt_Hora";
+            this.Txt_Hora.ReadOnly = true;
+            this.Txt_Hora.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Hora.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hora";
+            // 
+            // Txt_Meslestras
+            // 
+            this.Txt_Meslestras.Enabled = false;
+            this.Txt_Meslestras.Location = new System.Drawing.Point(429, 178);
+            this.Txt_Meslestras.Name = "Txt_Meslestras";
+            this.Txt_Meslestras.ReadOnly = true;
+            this.Txt_Meslestras.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Meslestras.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(327, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Mes en letras";
+            // 
+            // Txt_Dialetras
+            // 
+            this.Txt_Dialetras.Enabled = false;
+            this.Txt_Dialetras.Location = new System.Drawing.Point(429, 222);
+            this.Txt_Dialetras.Name = "Txt_Dialetras";
+            this.Txt_Dialetras.ReadOnly = true;
+            this.Txt_Dialetras.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Dialetras.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(327, 222);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Dia en letras";
+            // 
+            // Txt_Agregardias
+            // 
+            this.Txt_Agregardias.Location = new System.Drawing.Point(488, 288);
+            this.Txt_Agregardias.Name = "Txt_Agregardias";
+            this.Txt_Agregardias.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Agregardias.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(327, 291);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(126, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Agregar Dias A La Fecha";
+            // 
+            // Txt_Nuevafecha
+            // 
+            this.Txt_Nuevafecha.Enabled = false;
+            this.Txt_Nuevafecha.Location = new System.Drawing.Point(620, 288);
+            this.Txt_Nuevafecha.Name = "Txt_Nuevafecha";
+            this.Txt_Nuevafecha.ReadOnly = true;
+            this.Txt_Nuevafecha.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Nuevafecha.TabIndex = 17;
+            // 
+            // Txt_Nuevafechamenos
+            // 
+            this.Txt_Nuevafechamenos.Enabled = false;
+            this.Txt_Nuevafechamenos.Location = new System.Drawing.Point(620, 331);
+            this.Txt_Nuevafechamenos.Name = "Txt_Nuevafechamenos";
+            this.Txt_Nuevafechamenos.ReadOnly = true;
+            this.Txt_Nuevafechamenos.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Nuevafechamenos.TabIndex = 20;
+            // 
+            // Txt_Restardias
+            // 
+            this.Txt_Restardias.Location = new System.Drawing.Point(488, 331);
+            this.Txt_Restardias.Name = "Txt_Restardias";
+            this.Txt_Restardias.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Restardias.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(327, 334);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(120, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Restar Dias A La Fecha";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Lbl_Hora
+            // 
+            this.Lbl_Hora.AutoSize = true;
+            this.Lbl_Hora.Location = new System.Drawing.Point(535, 47);
+            this.Lbl_Hora.Name = "Lbl_Hora";
+            this.Lbl_Hora.Size = new System.Drawing.Size(41, 13);
+            this.Lbl_Hora.TabIndex = 21;
+            this.Lbl_Hora.Text = "label10";
+            // 
+            // Txt_Edad
+            // 
+            this.Txt_Edad.Enabled = false;
+            this.Txt_Edad.Location = new System.Drawing.Point(145, 361);
+            this.Txt_Edad.Name = "Txt_Edad";
+            this.Txt_Edad.ReadOnly = true;
+            this.Txt_Edad.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Edad.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(43, 364);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(32, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Edad";
+            // 
+            // FuncionesDeFecha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Txt_Edad);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Lbl_Hora);
+            this.Controls.Add(this.Txt_Nuevafechamenos);
+            this.Controls.Add(this.Txt_Restardias);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Txt_Nuevafecha);
+            this.Controls.Add(this.Txt_Agregardias);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Txt_Dialetras);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Txt_Meslestras);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Txt_Hora);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Txt_año);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Txt_Numeromes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Txt_Numerodia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Btn_Ejecutar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Dtp_Fecha);
+            this.Name = "FuncionesDeFecha";
+            this.Text = "Funciones de Fecha";
+            this.Load += new System.EventHandler(this.FuncionesDeFecha_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker Dtp_Fecha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_Ejecutar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txt_Numerodia;
+        private System.Windows.Forms.TextBox Txt_Numeromes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Txt_año;
+        private System.Windows.Forms.TextBox Txt_Hora;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Txt_Meslestras;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Txt_Dialetras;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Txt_Agregardias;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox Txt_Nuevafecha;
+        private System.Windows.Forms.TextBox Txt_Nuevafechamenos;
+        private System.Windows.Forms.TextBox Txt_Restardias;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Lbl_Hora;
+        private System.Windows.Forms.TextBox Txt_Edad;
+        private System.Windows.Forms.Label label10;
+    }
+}
---
+++ Programacion 3/Ejercicio 1/FuncionesDeFecha.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class FuncionesDeFecha : Form
+    {
+        public FuncionesDeFecha()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Ejecutar_Click(object sender, EventArgs e)
+        {
+            
+            DateTime FechaIngresada = Dtp_Fecha.Value;
+            Txt_Numerodia.Text = FechaIngresada.Day.ToString ();
+            Txt_Numeromes.Text = FechaIngresada.Month.ToString();
+            Txt_año.Text = FechaIngresada.Year.ToString();
+            Txt_Hora.Text = FechaIngresada.Hour.ToString();
+
+            Txt_Meslestras.Text = FechaIngresada.ToString("MMMMM");
+            Txt_Dialetras.Text = FechaIngresada.ToString("dddd");
+
+            int numerodias = Convert.ToInt32(Txt_Agregardias.Text);
+            Txt_Nuevafecha.Text = FechaIngresada.AddDays(numerodias).ToString();
+
+            int restarnumerodias = Convert.ToInt32(Txt_Restardias.Text);
+            Txt_Nuevafechamenos.Text = FechaIngresada.AddDays(restarnumerodias).ToString();
+            Txt_Edad.Text = DevolverEdad(FechaIngresada).ToString();
+        }
+
+        private int DevolverEdad(DateTime FechaNacimiento)
+        {
+            DateTime _fechanacimiento = FechaNacimiento;
+            DateTime _fechaactual = DateTime.Now;
+
+            int edad;
+
+            if(_fechanacimiento.Day > _fechaactual.Day )
+            {
+                return 0;
+            }
+            {
+                edad = _fechaactual.Year - _fechanacimiento.Year;
+                if (_fechanacimiento.Month > _fechaactual.Month )
+                {
+                    --edad;
+                }
+                return edad;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Lbl_Hora.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void FuncionesDeFecha_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+    }
+}
